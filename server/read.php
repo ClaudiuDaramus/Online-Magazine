5955472058bd433cc8d7869d4f7328465c5f0894
Claudiu Daramus <38111672+ClaudiuDaramus@users.noreply.github.com>
2020-11-25 10:56:08 +0200

    CRUD operations

---
+++ server/read.php
@@ -0,0 +1,64 @@
+<?php
+// SET HEADER
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json; charset=UTF-8");
+
+// INCLUDING DATABASE AND MAKING OBJECT
+require 'database.php';
+$db_connection = new Database();
+$conn = $db_connection->dbConnection();
+
+// CHECK GET ID PARAMETER OR NOT
+if(isset($_GET['id']))
+{
+    //IF HAS ID PARAMETER
+    $article_id = filter_var($_GET['id'], FILTER_VALIDATE_INT,[
+        'options' => [
+            'default' => 'all_articles',
+            'min_range' => 1
+        ]
+    ]);
+}
+else{
+    $article_id = 'all_articles';
+}
+
+// MAKE SQL QUERY
+// IF GET ARTICLES ID, THEN SHOW ARTICLES BY ID OTHERWISE SHOW ALL ARTICLES
+$sql = is_numeric($article_id) ? "SELECT * FROM `article` WHERE id='$article_id'" : "SELECT * FROM `article`"; 
+
+$stmt = $conn->prepare($sql);
+
+$stmt->execute();
+
+//CHECK WHETHER THERE IS ANY ARTICLE IN OUR DATABASE
+if($stmt->rowCount() > 0){
+    // CREATE ARTICLES ARRAY
+    $articles_array = [];
+    
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        
+        $article_data = [
+            'id' => $row['id'],
+            'title' => $row['title'],
+            'body' => html_entity_decode($row['body']),
+            'writer_id' => $row['writer_id'],
+            'popularity' => $row['popularity'],
+            'create_date' => $row['create_date']
+        ];
+        // PUSH ARTICLE DATA IN OUR $articles_array ARRAY
+        array_push($articles_array, $article_data);
+    }
+    //SHOW ARTICLE/ARTICLES IN JSON FORMAT
+    echo json_encode($articles_array);
+ 
+
+}
+else{
+    //IF THER IS NO ARTICLE IN OUR DATABASE
+    echo json_encode(['message'=>'No article found']);
+}
+?>
---
+++ server/update.php
@@ -0,0 +1,67 @@
+<?php
+// SET HEADER
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: PUT");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// INCLUDING DATABASE AND MAKING OBJECT
+require 'database.php';
+$db_connection = new Database();
+$conn = $db_connection->dbConnection();
+
+// GET DATA FORM REQUEST
+$data = json_decode(file_get_contents("php://input"));
+
+//CHECKING, IF ID AVAILABLE ON $data
+if(isset($data->id)){
+    
+    $msg['message'] = '';
+    $article_id = $data->id;
+    
+    //GET ARTICLE BY ID FROM DATABASE
+    $get_article = "SELECT * FROM `article` WHERE id=:article_id";
+    $get_stmt = $conn->prepare($get_article);
+    $get_stmt->bindValue(':article_id', $article_id,PDO::PARAM_INT);
+    $get_stmt->execute();
+    
+    
+    //CHECK WHETHER THERE IS ANY ARTICLE IN OUR DATABASE
+    if($get_stmt->rowCount() > 0){
+        
+        // FETCH ARTICLE FROM DATBASE 
+        $row = $get_stmt->fetch(PDO::FETCH_ASSOC);
+        
+        // CHECK, IF NEW UPDATE REQUEST DATA IS AVAILABLE THEN SET IT OTHERWISE SET OLD DATA
+        $article_title = isset($data->title) ? $data->title : $row['title'];
+        $article_body = isset($data->body) ? $data->body : $row['body'];
+        $article_writer_id = isset($data->writer_id) ? $data->writer_id : $row['writer_id'];
+        
+        $update_query = "UPDATE `article` SET title = :title, body = :body, writer_id = :writer_id
+        WHERE id = :id";
+        
+        $update_stmt = $conn->prepare($update_query);
+        
+        // DATA BINDING AND REMOVE SPECIAL CHARS AND REMOVE TAGS
+        $update_stmt->bindValue(':title', htmlspecialchars(strip_tags($article_title)),PDO::PARAM_STR);
+        $update_stmt->bindValue(':body', htmlspecialchars(strip_tags($article_body)),PDO::PARAM_STR);
+        $update_stmt->bindValue(':writer_id', htmlspecialchars(strip_tags($article_writer_id)),PDO::PARAM_STR);
+        $update_stmt->bindValue(':id', $article_id,PDO::PARAM_INT);
+        
+        
+        if($update_stmt->execute()){
+            $msg['message'] = 'Data updated successfully';
+        }else{
+            $msg['message'] = 'data not updated';
+        }   
+        
+    }
+    else{
+        $msg['message'] = 'Invlid ID';
+    }  
+    
+    echo  json_encode($msg);
+    
+}
+?>
