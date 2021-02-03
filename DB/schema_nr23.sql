450100ee975f2df85ffc8d75098ff16a2131c5fb
ClaudiuDaramus <claudiudaramus10@gmail.com>
2021-02-03 14:50:37 +0200

    schema

---
+++ DB/schema_nr23.sql
@@ -0,0 +1,5 @@
+peste(cod_peste,specie,culoare,masa,gen);
+acvariu(cod_acvariu,volum,material,an_fabricatie);
+client(cod_client,nume,oras,data_nastere);
+crescut(cod_peste, cod_acvariu, data_introducere, data_mutare);
+cumpara (cod_peste, cod_client,data_achiz, pret_achizitie);
